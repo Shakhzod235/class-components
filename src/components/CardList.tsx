0c4c1a24bbb61954892b92a69ea0a7b30b9174ae
Shakhzod235 <shakhzodikramov235@gmail.com>
2025-07-14 21:35:29 +0500

    feat: implement loader

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -9,6 +9,7 @@ interface CardInfo {
 
 interface State {
   cards: CardInfo[];
+  isLoading: boolean;
 }
 
 const API_URL = 'https://rickandmortyapi.com/api/character';
@@ -16,21 +17,29 @@ const API_URL = 'https://rickandmortyapi.com/api/character';
 export class CardList extends Component<unknown, State> {
   state: State = {
     cards: [],
+    isLoading: true,
   };
 
   async componentDidMount(): Promise<void> {
     try {
-      const response = await fetch(API_URL);
-      if (!response.ok) throw new Error(`Response status: ${response.status}`);
+      setTimeout(async () => {
+        const response = await fetch(API_URL);
+        if (!response.ok)
+          throw new Error(`Response status: ${response.status}`);
 
-      const data = await response.json();
-      console.log(data);
-      this.setState({
-        cards: data.results,
-      });
+        const data = await response.json();
+        console.log(data);
+        this.setState({
+          cards: data.results,
+          isLoading: false,
+        });
+      }, 1000);
     } catch (error: unknown) {
       if (error instanceof Error)
         console.error(`Ошибка загрузки: ${error.message}`);
+      this.setState({
+        isLoading: false,
+      });
     }
   }
 
@@ -38,17 +47,41 @@ export class CardList extends Component<unknown, State> {
     return (
       <div className="flex justify-center flex-col items-center w-100">
         {this.state.cards.length !== 0 && (
-          <div className="flex justify-between w-full">
+          <div className="flex justify-between w-full mb-6">
             <h3>Character Name</h3>
             <h3>Character Species</h3>
           </div>
         )}
-        {this.state.cards.length !== 0 ? (
+        {this.state.isLoading ? (
+          <h2 className="flex items-center">
+            <svg
+              className="mr-3 size-5 animate-spin text-black"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="opacity-25"
+                cx="12"
+                cy="12"
+                r="10"
+                stroke="currentColor"
+                strokeWidth="4"
+              ></circle>
+              <path
+                className="opacity-75"
+                fill="currentColor"
+                d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+              ></path>
+            </svg>
+            Loading...
+          </h2>
+        ) : this.state.cards.length === 0 ? (
+          <h2>Empty List</h2>
+        ) : (
           this.state.cards.map((card) => (
             <Card key={card.id} name={card.name} description={card.species} />
           ))
-        ) : (
-          <h1>Empty List</h1>
         )}
       </div>
     );
